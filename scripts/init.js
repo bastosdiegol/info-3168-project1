b86547110322cdf29d2a2b42ffca21d940a45e18
Diego Bastos <bastosdiegol@gmail.com>
2024-02-10 00:25:51 -0500

    MLBStatsAsyncHandler now const Object

--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,5 @@
 // Call that Initialize all the Select Options
-addEventListener("load", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const yearSelect = document.getElementById("year");
   addSelectOptions(yearSelect, 2020, 2022);
 
@@ -10,6 +10,43 @@ addEventListener("load", function () {
   addSelectOptions(daySelect, 1, 31);
 });
 
-// MLB Stats Test Request
-var MLBStats = new MLBStatsAsyncHandler();
-MLBStats.getStats(3, 6, 2019);
+/**
+ * Object that will Hanlde Asynchronous JSON Request to MLB Stats API.
+ * @constant
+ * @property {XMLHttpRequest} request Data API for AJAX.
+ * @property {Object} statsData Literal Object containing the JSON Response.
+ * @property {number} ISOK Status code for successful request
+ */
+const MLBStatsAsyncHandler = {
+  request: new XMLHttpRequest(),
+  statsData: null,
+  ISOK: 200,
+  /**
+   * Function that formats the URL and submit the JSON Request to MLB API.
+   * Asynchronous Request
+   * On CallBack the JSON response will be parsed to JavaScript Object statsData
+   * @method @async
+   * @param {number} day - Day of the matches.
+   * @param {number} month - Month of the matches.
+   * @param {number} year - Year of the matches.
+   */
+  getStats: function (day, month, year) {
+    let url = `https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&date=${day}/${month}/${year}`;
+    // Async anonymous arrow function to handle the response
+    this.request.onload = () => {
+      if (this.request.status === this.ISOK) {
+        this.statsData = JSON.parse(this.request.responseText);
+        // TODO: Remove following log
+        console.log(this.statsData);
+        // TODO: Set the matches into the HTML form
+      } else {
+        console.log("HTTP request error.");
+      }
+    };
+    this.request.open("GET", url, true);
+    this.request.send();
+  },
+};
+
+// Test Request
+MLBStatsAsyncHandler.getStats(3, 6, 2019);
--- scripts/utility.js
+++ scripts/utility.js
@@ -19,36 +19,3 @@ function addSelectOptions(selectElement, min, max) {
     selectElement.appendChild(option);
   }
 }
-
-/**
- * Object Constructor for MLB Asynchronous JSON Request.
- * @property {XMLHttpRequest} request Data API for AJAX.
- * @property {Object} statsData Literal Object containing the JSON Response.
- * @constant {number} ISOK Status code for successful request
- */
-function MLBStatsAsyncHandler() {
-  this.request = new XMLHttpRequest();
-  this.statsData = null;
-  const ISOK = 200;
-  /**
-   * Function that formats the URL and submit the JSON Request to MLB API.
-   * Asynchronous Request - JavaScript Object will be parsed to statsData
-   * @method @async {}
-   * @param {number} day - Day of the matches.
-   * @param {number} month - Month of the matches.
-   * @param {number} year - Year of the matches.
-   */
-  this.getStats = function (day, month, year) {
-    let url = `https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&date=${day}/${month}/${year}`;
-    // Async anonymous arrow function to handle the response
-    this.request.onload = () => {
-      if (this.request.status === ISOK) {
-        this.responseData = JSON.parse(this.request.responseText);
-        console.log(this.responseData);
-        // TODO: Set the matches into the HTML form
-      }
-    };
-    this.request.open("GET", url, true);
-    this.request.send();
-  };
-}
