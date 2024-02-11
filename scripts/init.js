7cbbb74dfc99c52bff022a6abc6f0b89c96cba37
Diego Bastos <bastosdiegol@gmail.com>
2024-02-10 19:55:04 -0500

    Form looping through games array

--- scripts/init.js
+++ scripts/init.js
@@ -11,42 +11,79 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 /**
- * Object that will Hanlde Asynchronous JSON Request to MLB Stats API.
- * @constant
+ * Global Index for the Games Object Array.
+ * @global @type {number}
+ */
+var gamesIndex = 0;
+
+/**
+ * Constant utilized to inform page logic which direction it should iterate, when navigating the JSON data.
+ * @constant {number} PREVIOUS - Constant that informs logic to iterate to PREVIOUS game match.
+ * @constant {number} NEXT - Constant that informs logic to iterate to NEXT game match.
+ */
+const GAME_NAV = Object.freeze({
+  PREVIOUS: 0,
+  NEXT: 1,
+});
+
+/**
+ * Object that will Handle Asynchronous JSON Request to MLB Stats API.
+ * @global @constant
  * @property {XMLHttpRequest} request Data API for AJAX.
- * @property {Object} statsData Literal Object containing the JSON Response.
+ * @property {Array<Object>} gamesObjArray Object array containing the games list from the JSON Response.
+ * @property {Map<Date, Object>} savedSearchMap Map containing all API requests done in a single session.
  * @property {number} ISOK Status code for successful request
  */
-const MLBStatsAsyncHandler = {
+const MLBStatsAPIHandler = {
   request: new XMLHttpRequest(),
-  statsData: null,
+  gamesObjArray: [],
+  savedSearchMap: new Map(),
   ISOK: 200,
   /**
    * Function that formats the URL and submit the JSON Request to MLB API.
    * Asynchronous Request
-   * On CallBack the JSON response will be parsed to JavaScript Object statsData
-   * @method @async
+   * On CallBack the JSON response will be parsed to JavaScript Object and saved
+   * The response games array object will be saved for web page navigation
+   * @method
    * @param {number} day - Day of the matches.
    * @param {number} month - Month of the matches.
    * @param {number} year - Year of the matches.
    */
   getStats: function (day, month, year) {
-    let url = `https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&date=${day}/${month}/${year}`;
-    // Async anonymous arrow function to handle the response
-    this.request.onload = () => {
-      if (this.request.status === this.ISOK) {
-        this.statsData = JSON.parse(this.request.responseText);
-        // TODO: Remove following log
-        console.log(this.statsData);
-        // TODO: Set the matches into the HTML form
-      } else {
-        console.log("HTTP request error.");
-      }
-    };
-    this.request.open("GET", url, true);
-    this.request.send();
+    let tempDate = new Date(year, month, day);
+    gamesIndex = 0;
+    // Checks if date passed by parameter was searched alredy
+    // To avoid sending the same request to the API in the current session
+    if (this.savedSearchMap.has(tempDate)) {
+      // Search found, get Map value and saves the Games Object Array
+      this.gamesObjArray = this.savedSearchMap.get(tempDate).dates[0].games;
+      console.log("Used previously saved search.");
+      // First Game Match Info to the Page
+      displayGameInfo(this.gamesObjArray[gamesIndex]);
+    } else {
+      let url = `https://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&date=${day}/${month}/${year}`;
+      /**
+       * Async anonymous arrow function to handle the response.
+       * @function @async
+       */
+      this.request.onload = () => {
+        if (this.request.status === this.ISOK) {
+          // Convert the Response to Object and Saves it.
+          let statsData = JSON.parse(this.request.responseText);
+          this.savedSearchMap.set(tempDate, statsData);
+          // Updates the current Games Object Array
+          this.gamesObjArray = statsData.dates[0].games;
+          // First Game Match Info to the Page
+          displayGameInfo(this.gamesObjArray[gamesIndex]);
+        } else {
+          console.log("HTTP request error.");
+        }
+      };
+      this.request.open("GET", url, true);
+      this.request.send();
+    }
   },
 };
 
 // Test Request
-MLBStatsAsyncHandler.getStats(3, 6, 2019);
+MLBStatsAPIHandler.getStats(3, 6, 2019);
--- scripts/utility.js
+++ scripts/utility.js
@@ -19,3 +19,38 @@ function addSelectOptions(selectElement, min, max) {
     selectElement.appendChild(option);
   }
 }
+
+/**
+ * Function that iterates through game matches and display their information on the page.
+ * @param {GAME_NAV} direction - Constant that defines which match to show: previous or next
+ */
+function changeMatch(direction) {
+  switch (direction) {
+    case GAME_NAV.PREVIOUS:
+      // If looped through the initial array position, resets to the array tail
+      if (--gamesIndex < 0) {
+        gamesIndex = MLBStatsAPIHandler.gamesObjArray.length - 1;
+      }
+      break;
+    case GAME_NAV.NEXT:
+      // If looped through the final array position, resets to the array head
+      if (++gamesIndex >= MLBStatsAPIHandler.gamesObjArray.length) {
+        gamesIndex = 0;
+      }
+      break;
+    default:
+      console.log("Invalid navigation option.");
+      return;
+  }
+  // Display New Match Info on the Form
+  displayGameInfo(MLBStatsAPIHandler.gamesObjArray[gamesIndex]);
+}
+
+/**
+ * Function that will receive a Game Object and set its informations on the HTML Form.
+ * @param {Object} game - JSON Object containing all information related to a game.
+ */
+function displayGameInfo(game) {
+  console.log(game);
+  // TODO
+}
